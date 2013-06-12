54f708dd363875047123debdb73c9a956bc4b7ff
Brett Mack <brett.mack@foxienet.com>
2013-06-12 13:59:28 +0100

    Added functionality to fetch an xml rss feed, parse the data, then list each of the episodes with the relevant link to the audio file

--- addon.py
+++ addon.py
@@ -9,19 +9,59 @@
 '''
 
 from xbmcswift2 import Plugin
-
+from urllib2 import urlopen
+from xml.dom import minidom
 
 plugin = Plugin()
+
+def get_tag(item, tag_name):
+    return item.getElementsByTagName(tag_name)[0]
+
+def get_tag_value(item, tag_name):
+    return get_tag(item, tag_name).childNodes[0].toxml()
+
+def get_tag_attribute(tag, attr_name):
+    return tag.getAttribute(attr_name)
+
+
+def get_feed():
+	'''Gets the rss feed from feedburner'''
+	rss = urlopen('http://feeds.feedburner.com/Ruby5?format=xml')
+	return rss
+
+def get_episodes():
+	'''Grabs each of the episodes listed in the rss feed'''
+	feed = get_feed()
+
+	xmldom = minidom.parse(feed)
+	channel_node = xmldom.childNodes[2]
+
+	episodes = []
+
+	_items = channel_node.getElementsByTagName('item')
+
+	for i in range(0, _items.length):
+		episodes.append({
+            'title': get_tag_value(_items[i], 'title') + ' (' + get_tag_value(_items[i], 'itunes:duration') + ')',
+            'description': get_tag_value(_items[i], 'description'),
+            'url': get_tag_attribute(get_tag(_items[i], 'enclosure'), 'url')
+        })
+
+    	return episodes
 
 
 @plugin.route('/')
 def index():
-    item = {
-        'label': 'Hello XBMC!',
-        'path': 'http://s3.amazonaws.com/KA-youtube-converted/JwO_25S_eWE.mp4/JwO_25S_eWE.mp4',
-        'is_playable': True
-    }
-    return [item]
+    items = [{
+        'label': episode['title'],
+        'path': episode['url'],
+        'info': {
+        	'plot': episode['description']
+        },
+        'is_playable': True,
+    } for episode in get_episodes()]
+
+    return items
 
 
 if __name__ == '__main__':
